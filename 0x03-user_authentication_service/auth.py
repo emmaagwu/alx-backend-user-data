8ba4248cd962e95612ef9efe9915e502d0370c73
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-08-13 18:10:52 +0100

    add authentication-related routines

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,7 +40,9 @@ class Auth:
         """Check if a user's login details are valid."""
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+            return bcrypt.checkpw(
+                    password.encode("utf-8"), user.hashed_password
+                    )
         except NoResultFound:
             return False
 
